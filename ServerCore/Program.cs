d3fb215c9d419ddcf78efd53c3e9a13a5545c1ae
suminlee <suminpixel@gmail.com>
2020-07-07 13:13:52 +0900

    memory barrier & racecondition

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,75 +5,55 @@ using System.Threading.Tasks;
 namespace ServerCore
 {
 
+
     class Program
     {
-        volatile static bool _stop = false; //전역변수는 모든 쓰레드에서 동시접근
 
-        //tip : volatile(볼라틱 키워드)는 c#에서 해당 변수가 포함된 코드를 변형 및 최적화 하지 말라는 표기
-        //릴리즈 모드와 개발 모드가 달라, 컴파일러에 의해 최적화가 되고나면 코드가 변형된다. 특히 멀티쓰레딩에서 무한루프에 빠질 가능성이 있다.
-        //볼라틱 키워드는 언어별로 스펙이 다르기때문에 쓰지말고 매모리배리어 등으로 대체
-        static void ThreadMain(){
-            Console.WriteLine("쓰레드 시작!");
-            while(_stop == false){
-                //누군가가 stop 신호를 주길 기다림
-            }
-            Console.WriteLine("쓰레드 종료!");
-        }
-        static void MainTread(object state){
+        static int number = 0;
 
-            
-            for(int i = 0; i < 5; ++i){
-                Console.WriteLine("Hello Thread!");
+        static void Thread_1(){
+            for(int i = 0; i < 10000; i++){
+                Interlocked.Increment(ref number); //ref : 변수의 참조(주소)값
+                // tip: interlocked : number을 여러과정으로 += 하지 않고 한번에 -> race condition 문제 해결
+                // 동시다발경합 X 원자적으로 실행순서 보장됨. 그러나 그만큼 다른 쓰레드가 자원을 돌려(공유)받기 위한 대기시간을 가짐 -> 느림.
+
+                //실제로 일어나는 number 변수 증가
+                /*
+                int temp = number; 
+                temp += 1; 
+                number = temp; 
+                */
+             
             }
-          
         }
+
+        static void Thread_2(){
+            for(int i = 0; i < 10000; i++){
+                
+                Interlocked.Decrement(ref number);
+            }
+        }
+
+        
         static void Main(string[] args)
         {
-            Task t = new Task(()=> {while(true){}}, TaskCreationOptions.LongRunning); //TaskCreationOptions.LongRunning : 오래걸리는 일이라는 것을 명시, 워커 쓰레드를 소모하지 않고 
-            t.Start();
-            //tip: Task => 쓰레드가 할 일의 단위를 정의해서 사용 //쓰레드풀 무한 대기 를 해결하는 방법중 하나
+            
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
 
-            Thread.Sleep(1000);
-            _stop = true;
+                Task.WaitAll(t1, t2);
+                Console.WriteLine(number);
 
-            Console.WriteLine("Stop 호출!");
-            Console.WriteLine("종료 대기중!");
-            t.Wait(); //태스크가 끝날때까지 기다림
-            Console.WriteLine("종료 성공!");
+                //tip: race condition 
+                //공유자원에 다수의 접근권한이 동시에 부여되면서, 
+                //조작순서, 연산 등에 오작동을 일으키는 문제
 
-
-        
-            /*
-            ThreadPool.SetMinThreads(1, 1); // 최소 쓰레드 수
-            ThreadPool.SetMaxThreads(5, 5); // 최대 쓰레드 수
-            for(int i = 0; i < 5; ++i){
-                ThreadPool.QueueUserWorkItem((obj)=>{while (true){ }}); // <- 이런식으로 만들면 쓰레드(워커)가 영영 돌아오지 않음
-            }
-
-            ThreadPool.QueueUserWorkItem(MainTread); //파라매터로 WaitCallback
-            //tip: C# 쓰레드 풀
-            //쓰레드를 직접 만들어 관리하는게 부담스럽다면, 쓰레드 풀을 사용
-            //풀이라는 공간에 잠시 가능한 쓰레드들을 대기시켜 두었다가 필요할때 사용할 수 있도록 함
-            //기본은 백그라운드로 실행한다.
-            //스레드를 필요할때마다 그때그때 만든다는것은 상당한자원을 소모하는 일이기 때문
-            //하지만 쓰레드 풀도 먹통이 될 수 있음
-            */
-
-
-            /*
-            //전통적인 쓰레드 생성법
-            Thread t = new Thread(MainTread); //기본 : 포그라운드 스레드 생성
-            t.IsBackground = true;
-            t.Start(); //생성된 쓰레드는 함수가 종료되면 소멸 
-            Console.WriteLine("Wating for Thread!");
-        
-            t.Join(); //스레드가 끝날때까지 기다린 후 다음 코드 실행
-
-            Console.WriteLine("Hello World!");
-            */
-            while(true){
+                //해결 방법 -> atomic 원자적으로 덧셈 뺄셈할것 (Interlocked)
+                //A의 집행검을 인벤에서 빼라
+                //B의 집행검을 인벤에서 넣어라
                 
-            }
         }
     }
 }
---
+++ ServerCore/lecture/Program2.cs
@@ -0,0 +1,63 @@
+﻿/* using System;
+using System.Threading;
+using System.Threading.Tasks;
+// 핵심 기능
+namespace ServerCore
+{
+
+    class Program
+    {
+        static volatile int x = 0;
+        static volatile int y = 0;
+        static volatile int r1 = 0;
+        static volatile int r2 = 0;
+
+
+        static void Thread_1(){
+            y = 1; // Store y 
+            // tip:  메모리 베리어 > 멀티 쓰레딩 환경에서는 명령 순서가 바뀔 수 있으므로 메모리 배리어 를 씀
+            // 쓰는 이유 = 코드 재 배치 방지 / 가시성
+            // 종류 1. Full Memory Barrier (변수 store/ load 동작의 코드 재배치 둘다 막음)
+            // 종류 2. Store Memory Barrier (store 만 막음)
+            // 종류 3. Load Memory Barrier (load 만 막음)
+
+            Thread.MemoryBarrier();
+
+            r1 = x; // Load x
+        }
+
+        static void Thread_2(){
+
+            x = 1;
+            
+            Thread.MemoryBarrier(); //메모리 베리어 
+
+            r2 = y;
+        }
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while(true){
+                count ++ ;
+                x =  y = r1 = r1 = 0;
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+                if(r1== 0 && r2==0){
+                    break;
+                }
+            }
+            Console.WriteLine($"{count} 번 만에 빠져");
+            //tip: cpu (하드웨어) 의 최적화
+            //x, y, r1, r2 등의 값 변형 등이 의존성이 없는 
+            //없는 경우 자기멋대로 함수실행 , 명령 순서 등을 뒤집어서 최적화를 실행한다던지 최적화를 한다.
+
+            //-> 멀티 쓰레딩 환경에서는 명령 순서가 꼬이는것이 매우 중요한 이슈이다.
+        }
+    }
+}
+ */
